1dccce0d316c970ce4a0a42285c73e023a9a34df
Talal Abbas <talalabbas84@gmail.com>
2020-03-11 03:45:58 +0500

    Adds rental detail and rental listing page. Adds bootstrap and scss. Adds redux architecture and routing.

---
+++ src/components/rental/rental-detail/RentalDetail.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import * as actions from 'actions';
+
+class RentalDetail extends Component {
+  componentDidMount() {
+    const rentalId = this.props.match.params.id;
+    //Dispatch actionS
+    this.props.dispatch(actions.fetchRentalById(rentalId));
+  }
+
+  render() {
+    const rental = this.props.rental;
+    if (rental.id) {
+      return (
+        <div>
+          <h1>{rental.title} </h1>
+          <h1>{rental.city} </h1>
+          <h1>{rental.description} </h1>
+          <h1>{rental.dailyRate} </h1>
+        </div>
+      );
+    } else {
+      return <h1>Loading...</h1>;
+    }
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    rental: state.rental.data
+  };
+}
+
+export default connect(mapStateToProps)(RentalDetail);
